68f2601bba63375571147fa135bd3332c7bd5559
wrathchild14 <jovanprodanov123@gmail.com>
2024-05-21 22:32:27 +0200

    fix snake actions, add random actions script

---
+++ rl/random_actions.py
@@ -0,0 +1,29 @@
+import mlagents
+from mlagents_envs.environment import UnityEnvironment
+
+env = UnityEnvironment(file_name="unity_builds/snake", seed=1, side_channels=[])
+env.reset()
+
+behaviour_name = list(env.behavior_specs)[0]
+spec = env.behavior_specs[behaviour_name]
+
+done = False
+
+while not done:
+    decision_steps, terminal_steps = env.get_steps(behaviour_name)
+    print(decision_steps.obs[0])
+
+    if len(decision_steps) == 0:
+        break
+
+    tracked_agent = decision_steps.agent_id[0]
+    action = spec.action_spec.random_action(len(decision_steps))
+    
+    env.set_actions(behaviour_name, action)
+    env.step()
+
+    if tracked_agent in terminal_steps:
+        done = True
+
+print("Game over")
+env.close()
