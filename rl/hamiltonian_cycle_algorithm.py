6e0409488c9cf48476d78ba3bc959008017a7ed2
Rok Nikolič <rok.nikolic@gmail.com>
2024-05-25 19:59:37 +0200

    hamiltonian cycle function done

--- rl/hamiltonian_cycle_algorithm.py
+++ rl/hamiltonian_cycle_algorithm.py
@@ -1,5 +1,4 @@
 from random import choice
-import numpy as np
 
 # Direction
 LEFT = 0
@@ -35,7 +34,7 @@ def generate_valid_prims_edges(visited, rows, columns):
     return current_valid_edges
 
 
-def get_direction(node1, node2):
+def get_direction_too_adjacent_node(node1, node2):
     y1, x1 = node1
     y2, x2 = node2
     if x2 < x1:
@@ -48,7 +47,7 @@ def get_direction(node1, node2):
         return DOWN
 
 
-def prims_grid_algorithm(rows, columns):
+def prims_algorithm_for_grid(rows, columns):
     """
     generates minimum spanning tree for a grid of a size (rows, columns)
     """
@@ -65,10 +64,10 @@ def prims_grid_algorithm(rows, columns):
         visited.append(node2)
         queue.remove(node2)
 
-        direction1 = get_direction(node1, node2)
-        minimum_spanning_tree[node1[0]][node1[1]].append(direction1)
-        direction2 = get_direction(node2, node1)
-        minimum_spanning_tree[node2[0]][node2[1]].append(direction2)
+        direction1to2 = get_direction_too_adjacent_node(node1, node2)
+        minimum_spanning_tree[node1[0]][node1[1]].append(direction1to2)
+        direction2to1 = get_direction_too_adjacent_node(node2, node1)
+        minimum_spanning_tree[node2[0]][node2[1]].append(direction2to1)
 
     return minimum_spanning_tree
 
@@ -93,31 +92,95 @@ def get_next_node(current_node, direction_of_travel):
 
 
 def traverse_grid(grid):
+    """
+    traverse the grid and return the coordinates of the path
+    """
     current_direction = RIGHT
     current_node = [0, 0]
-    path = []
-    for _ in range(len(grid) * len(grid[0]) * 2 - 1):
-        path.append(current_node)
+    mst_path = []
+    directions = []
+    for _ in range(len(grid) * len(grid[0]) * 2 - 2):
+        mst_path.append(current_node)
         current_node_connections = grid[current_node[0]][current_node[1]]
         current_direction = turn_left(current_direction)
         if current_direction in current_node_connections:
             current_node = get_next_node(current_node, current_direction)
+            directions.append(current_direction)
         else:
             for i in range(3):
                 current_direction = get_next_direction(current_direction)
                 if current_direction in current_node_connections:
                     current_node = get_next_node(current_node, current_direction)
+                    directions.append(current_direction)
                     break
-    return path
+    return mst_path, directions
 
 
-def hamiltonian_grid_cycle(rows, columns):
+def get_path_between_nodes(current_node, direction1, direction2):
+    y, x = current_node
+    path = []
+    # First LEFT
+    if direction1 == LEFT and direction2 == LEFT:
+        path.extend([[y, x - 1], [y, x - 2]])
+    elif direction1 == LEFT and direction2 == UP:
+        path.extend([[y, x - 1], [y - 1, x - 1], [y - 2, x - 1]])
+    elif direction1 == LEFT and direction2 == RIGHT:
+        path.extend([[y, x - 1], [y - 1, x - 1], [y - 1, x], [y - 1, x + 1]])
+    elif direction1 == LEFT and direction2 == DOWN:
+        path.extend([[y + 1, x]])
+    # First UP
+    elif direction1 == UP and direction2 == LEFT:
+        path.extend([[y, x - 1]])
+    elif direction1 == UP and direction2 == UP:
+        path.extend([[y - 1, x], [y - 2, x]])
+    elif direction1 == UP and direction2 == RIGHT:
+        path.extend([[y - 1, x], [y - 1, x + 1], [y - 1, x + 2]])
+    elif direction1 == UP and direction2 == DOWN:
+        path.extend([[y - 1, x], [y - 1, x + 1], [y, x + 1], [y + 1, x + 1]])
+    # First RIGHT
+    elif direction1 == RIGHT and direction2 == LEFT:
+        path.extend([[y, x + 1], [y + 1, x + 1], [y + 1, x], [y + 1, x - 1]])
+    elif direction1 == RIGHT and direction2 == UP:
+        path.extend([[y - 1, x]])
+    elif direction1 == RIGHT and direction2 == RIGHT:
+        path.extend([[y, x + 1], [y, x + 2]])
+    elif direction1 == RIGHT and direction2 == DOWN:
+        path.extend([[y, x + 1], [y + 1, x + 1], [y + 2, x + 1]])
+    # First DOWN
+    elif direction1 == DOWN and direction2 == LEFT:
+        path.extend([[y + 1, x], [y + 1, x - 1], [y + 1, x - 2]])
+    elif direction1 == DOWN and direction2 == UP:
+        path.extend([[y + 1, x], [y + 1, x - 1], [y, x - 1], [y - 1, x - 1]])
+    elif direction1 == DOWN and direction2 == RIGHT:
+        path.extend([[y, x + 1]])
+    elif direction1 == DOWN and direction2 == DOWN:
+        path.extend([[y + 1, x], [y + 2, x]])
+
+    current_node = path[-1]
+    return path, current_node
+
+
+def generate_hamiltonian_path(directions):
+    current_direction = RIGHT
+    current_node = [0, 0]
+    hamiltonian_path = [current_node]
+    for direction in directions:
+        paths, current_node = get_path_between_nodes(current_node, current_direction, direction)
+        current_direction = direction
+        hamiltonian_path.extend(paths)
+    return hamiltonian_path
+
+
+def hamiltonian_cycle_for_grid(rows, columns):
     """
     generates a hamiltonian cycle for a grid of a size (rows, columns)
     """
-    min_spanning_tree = prims_grid_algorithm(rows, columns)
-    print(min_spanning_tree)
-    print(traverse_grid(min_spanning_tree))
+    min_spanning_tree = prims_algorithm_for_grid(rows, columns)
+    mst_path, directions = traverse_grid(min_spanning_tree)
+    hamiltonian_path = generate_hamiltonian_path(directions)
+    return hamiltonian_path
 
 
-hamiltonian_grid_cycle(2, 3)
+if __name__ == '__main__':
+    cycle = hamiltonian_cycle_for_grid(2, 2)
+    print(cycle)
